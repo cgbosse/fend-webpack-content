8e107b9be9cf4ea425444a626b89e6201646c9bc
cgbosse <cgbosse@gmail.com>
2021-11-05 16:40:03 +0000

    Steps 5
    Steps followed as indicated

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
@@ -19,6 +20,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new CleanWebpackPlugin()
     ]
 }
